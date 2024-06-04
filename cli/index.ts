0cdaa2524525e958aa2d2e61c033a99ee9e3e5e8
ranjabi <fikriranjabi02@gmail.com>
2024-06-04 14:55:07 +0700

    feat: display fail parse error

--- cli/index.ts
+++ cli/index.ts
@@ -19,9 +19,24 @@ program.command('parse')
     const getFileExtension = (filename) => filename.split('.').pop()
     const allowedExtensions = new Set(['tsx'])
 
+    let isPagesFolderFound = false
+    let isUsingNextPackage = false
+
     function rec_file(folderPath, prefix, res) {
       const dirs = fs.readdirSync(folderPath, { withFileTypes: true })
       dirs.forEach((dir) => {
+        const fullPath = (prefix + '/' + dir.name).substring(1)
+        if (dir.name.endsWith('package.json')) {
+          const content = fs.readFileSync(fullPath, 'utf8')
+          if (content.includes('"next"')) {
+            isUsingNextPackage = true
+          }
+        }
+
+        if (fullPath === 'pages' || fullPath === 'src/pages') {
+          isPagesFolderFound = true
+        }
+
         if (dir.isDirectory() && dir.name !== 'node_modules' && !dir.name.startsWith('.')) {
           rec_file(path.join(dir.path, dir.name), prefix + '/' + dir.name, res)
         }
@@ -36,21 +51,30 @@ program.command('parse')
           } catch (err) {
             console.error(err);
           }
-
         }
       })
     }
 
     const currentPath = process.cwd()
     console.log('Start parsing from: ', currentPath)
+
     let dirRes = [] as RawFile[];
     rec_file(currentPath, '', dirRes)
+
+    if (!isUsingNextPackage) {
+      console.log('Fail to parse: Next.js not found in package')
+      return
+    }
+
+    if (!isPagesFolderFound) {
+      console.log('Fail to parse: Pages folder not found')
+      return
+    }
 
     console.log('Starting to write the result to data.json')
     console.log('Writing to:', config.rawFileOutputPath)
 
     try {
-      // fs.writeFileSync('/Users/ranjabi/Desktop/Coding/jabi-next-visualizer/app/.visualizer/data.json', JSON.stringify(dirRes));
       fs.writeFileSync(config.rawFileOutputPath, JSON.stringify(dirRes));
     } catch (err) {
       console.error(err);
@@ -58,12 +82,6 @@ program.command('parse')
 
     console.log('Parse finished')
   });
-
-// program.command('curdir')
-//   .description('Curent dir from project directory')
-//   .action(() => {
-//     console.log("Curent dir from project directory:", process.cwd());
-//   });
 
 program.command('config')
   .description('Show user config')
